45c2c1c81406aa5fab5e7943af8bd7bd43ecf678
brucelee03 <rohit.prajapati25603@gmail.com>
2024-08-20 09:27:16 +0000

    Fix the Error & Add Loader

---
+++ src/components/Header/index.js
@@ -0,0 +1,35 @@
+import {Link} from 'react-router-dom'
+
+import './index.css'
+
+const Header = () => (
+  <nav className="nav-header">
+    <div className="logo-card">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/wave-logo-img.png"
+        alt="wave"
+        className="logo"
+      />
+      <p className="app-name">Wave</p>
+    </div>
+    <ul className="nav-menu">
+      <li>
+        <Link className="nav-link" to="/">
+          Home
+        </Link>
+      </li>
+      <li>
+        <Link className="nav-link" to="/about">
+          About
+        </Link>
+      </li>
+      <li>
+        <Link className="nav-link" to="/contact">
+          Contact
+        </Link>
+      </li>
+    </ul>
+  </nav>
+)
+
+export default Header
